b8912674d70f361d47cf84a2c4f0b0c50944b65d
karmakarmeghdip <karmakarmeghdip@gmail.com>
2024-06-13 16:34:12 +0530

    updated content

--- quartz.config.ts
+++ quartz.config.ts
@@ -13,7 +13,7 @@ const config: QuartzConfig = {
     enablePopovers: true,
     analytics: null,
     locale: "en-US",
-    baseUrl: "https://karmakarmeghdip.github.io",
+    baseUrl: "https://meghdip.is-a.dev",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
     theme: {
